309c1fd25b8ad0982f71cbe108c55db48e97fd4e
Yuna Morgenstern <io@yuna.berlin>
2021-12-22 16:15:13 +0100

    style: exchanged logo

--- src/main/java/berlin/yuna/natsserver/junit/logic/NatsServer.java
+++ src/main/java/berlin/yuna/natsserver/junit/logic/NatsServer.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
